294da4c1c12ea42ca609c8488955b5207386a5d6
Kris <christellelocque@hotmail.fr>
2017-12-07 16:23:44 +0100

    envoi de l'exercice après résolution du problème

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('have fun !');
+function formulaire () {
+
+	var obj = {
+		first:$('#first_name').val(),
+	    name:$('#last_name').val(),
+		ville:$('#city').val()
+	};
+	console.log(obj.first); 
+	$('#username').html(obj.first +' '+ obj.name +' '+ obj.ville);
+}
+
+$("button").click(formulaire);
